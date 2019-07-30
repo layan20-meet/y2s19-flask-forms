1e5155858f1ec8608404d5d27c905486c407ea52
myname21-meet <myname21@meet.mit.edu>
2019-07-30 06:36:18 -0400

    finished all the labs git add .!

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request,redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,6 +11,25 @@ def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
 #Create an '/add' route here:
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_student_route():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        name=request.form['student_name']
+        year=request.form['student_year']
+        student1=add_student(name,year)
+        return render_template('add.html')
+
+
+@app.route('/delete/<int:student_id>', methods=['POST','GET'])
+def delete(student_id):
+	if request.method == 'POST':
+		delete_student_id(student_id)
+		print("deleted")
+	return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
